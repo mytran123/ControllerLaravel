9d34bb759bbf78f6367730742c466c49d52ad726
mytran123 <mytran091196@gmai.com>
2021-12-16 09:42:01 +0700

    thuchanh

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/email', function () {
+    return view('email.index');
+});
+
+Route::post('/check-email', [\App\Http\Controllers\UserController::class,'validationEmail'])->name('checkMail');
